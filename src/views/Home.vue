6d6f8cbd5f619a9f114ffa5f5f65e3f1af93ebbc
michael <michael.gallipo@gmail.com>
2021-08-27 21:59:36 -0400

    Initial Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+			<img
+				class="headerImage"
+				id="leftImage"
+				alt="Hippo logo"
+				src="./assets/images/hippo.jpg"
+			/>
+			<p class="title">Internet Hippo's Quote of the Day</p>
+			<img
+				class="headerImage"
+				id="rightImage"
+				alt="Hippo logo"
+				src="./assets/images/hippo.jpg"
+			/>
+		</header>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
+
+<style scoped>
+
+.headerImage {
+	width: 150px;
+	height: 150px;
+	position: absolute;
+	padding: 20px;
+}
+#leftImage {
+	top: 0px;
+	left: 0px;
+}
+#rightImage {
+	top: 0px;
+	right: 0px;
+}
+
+.title {
+	font-size: 46px;
+	font-weight: bold;
+	text-align: center;
+	font-family: cursive;
+}
+
+@media screen and (max-width: 950px) {
+	.headerImage {
+		position: relative;
+	}
+	#rightImage {
+		display: none;
+	}
+}
+</style>
