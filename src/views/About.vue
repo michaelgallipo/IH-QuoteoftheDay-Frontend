34af792a7f6e30cd8415d6edcb8cf4b14dd21232
michael <michael.gallipo@gmail.com>
2021-08-28 13:37:22 -0400

    Added functionality to change active quote

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,60 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Admin Functions</h1>
+    <button id="newQuote" v-on:click="newQuote">Send Quote</button>
   </div>
 </template>
+
+<script>
+const axios = require("axios");
+const qs = require("qs");
+
+export default {
+  name: 'about',
+  // components: {
+  //   HelloWorld
+  // }
+  data: function() {
+    return {
+      quotes: [],
+      activeQuote: "",
+      phoneNumbers: [],
+      error: ""
+    };
+  },
+  created: async function() {
+    await axios.get("/quotes").then(response => {
+      this.quotes = response.data;
+      this.activeQuote = this.quotes.filter(entry => entry.Active == true)
+    })
+    await axios.get("numbers").then(response => {
+      this.phoneNumbers = response.data
+    })
+  },
+  methods: {
+    newQuote: async function() {
+      const unusedQuotes = this.quotes.filter(elem => elem.Active == false && elem.Used == false);
+      let random = Math.floor(Math.random() * unusedQuotes.length);
+      const newQuote = unusedQuotes[random];
+      console.log(newQuote);
+      const chgStatus = {Used: true, Active: false};
+      await axios.put("/quotes/" + this.activeQuote[0]._id, qs.stringify(chgStatus))
+        .then(response => {
+          if (response.status >= 200 && response.status <= 210) {
+            chgStatus = {Active: true};
+            console.log(chgStatus);
+          }
+        })
+        .catch(err => {
+          this.error = err;
+        })
+      await axios.put("/quotes/" + newQuote._id, qs.stringify({Active: true}))
+        .then(response => {
+          if (response.status === 200) {
+              window.alert('Quote Updated');
+          }
+        })
+    }
+  }
+}
+</script>
